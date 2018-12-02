a3e666441ae5811d3c6220f6b8d473c33716e396
ShingJan <yuanj.shi@hotmail.com>
2018-12-02 01:29:35 -0600

    ahuh

--- spark_app/spark_pagerank.py
+++ spark_app/spark_pagerank.py
@@ -6,11 +6,6 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 
-def transform_to_url_tuple(string):
-    urls = string.split('\t')
-    return (urls[0], urls[1:])
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: hdfs_pagerank.py <directory>", file=sys.stderr)
@@ -18,20 +13,13 @@ if __name__ == "__main__":
 
     sc = SparkContext(appName="PythonStreamingPageRank")
     ssc = StreamingContext(sc, 10)
-
+    # 1 2 3 4; 2 3 5;
     lines = ssc.textFileStream(sys.argv[1])
     links = lines.filter(lambda l: len(l.split('\t')) > 1) \
-        .map(transform_to_url_tuple) \
-        .groupByKey()
-    ranks = links.map(lambda (name, neighbours): (name, 0.0))
-
-    contribs = links \
-        .join(ranks) \
-        .flatMap(
-        lambda (name, (neighbours, score)): map(lambda neighbour: (neighbour, 1 / len(neighbours)), neighbours))
-
-    ranks = contribs \
-        .reduceByKey(lambda score_by_a, score_by_b: score_by_a + score_by_b) \
+        .map(lambda line: line.split("\t"))
+    # [[2,3,4]; [3,5]]
+    contribs = links.flatMap(lambda neighbors: map(lambda neighbor: (neighbor, 1 / len(neighbors)), neighbors))
+    ranks = contribs.reduceByKey(lambda score_by_a, score_by_b: score_by_a + score_by_b)
 
     ranks.saveAsTextFiles("pr_output")
     ranks.pprint()
